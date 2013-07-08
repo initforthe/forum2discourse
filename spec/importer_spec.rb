f68cad5137c178f8ab10596edf08b4e16e9357f4
Tom Russell <tom.russell@initforthe.com>
2013-07-08 11:25:11 +0100

    Addition of importer spec with pending tests

---
+++ spec/importer_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'forum2discourse'
+require 'forum2discourse/importer'
+
+describe Forum2Discourse::Importer do
+  describe "#import" do
+    pending
+  end
+
+  describe "#import_topic" do
+    pending
+  end
+
+  describe "#find_or_create_category" do
+    pending
+  end
+
+  describe "#import_topic_posts" do
+    pending
+  end
+end
