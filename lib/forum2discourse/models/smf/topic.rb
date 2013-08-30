156b2e345dc708547ec2caca2792ebef129e613f
arturo <arturo@openframeworks.cc>
2013-08-30 14:42:02 +0200

    smf topic: set dates from first and last posts

--- lib/forum2discourse/models/smf/topic.rb
+++ lib/forum2discourse/models/smf/topic.rb
@@ -14,10 +14,14 @@ class Forum2Discourse::Models::SMF::Topic
   def to_discourse
     return nil if board == nil?
     subject = messages.first==nil ? "" : messages.first.subject
+    first_post_created_at = messages.first==nil ? nil : messages.first.created_at
+    last_post_created_at = messages.last==nil ? nil : messages.last.created_at
     Forum2Discourse::Models::Discourse::Topic.new({
       category: board.subject,
       posts: messages.map(&:to_discourse),
-      title: subject
+      title: subject,
+      created_at: first_post_created_at,
+      updated_at: last_post_created_at
     })
   end
 end
