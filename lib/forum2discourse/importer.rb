f4b5b9d193c8402fa8305356ef76383227618144
arturo <arturo@openframeworks.cc>
2013-08-31 00:11:27 +0200

    trying to make 1 by 1 work

--- lib/forum2discourse/importer.rb
+++ lib/forum2discourse/importer.rb
@@ -31,10 +31,10 @@ class Forum2Discourse::Importer
     find_or_create_category(user, topic.category)
     discourse_topic = TopicCreator.new(user, guardian, topic.serialize).create
     import_topic_posts(discourse_topic, topic.posts)
-  rescue
-    puts "FAILED TO IMPORT TOPIC #{topic.title}"
-    puts "Error: #{$!.message}"
-    puts $!.backtrace.join("\n")
+  #rescue
+  #  puts "FAILED TO IMPORT TOPIC #{topic.title}"
+  #  puts "Error: #{$!.message}"
+  #  puts $!.backtrace.join("\n")
   end
 
   def find_or_create_category(user, category)
