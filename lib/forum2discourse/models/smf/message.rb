f961532e1b44539e460c2d82781800d387db1ec7
arturo <arturo@openframeworks.cc>
2013-10-26 02:19:13 +0200

    comment attachments

--- lib/forum2discourse/models/smf/message.rb
+++ lib/forum2discourse/models/smf/message.rb
@@ -15,7 +15,7 @@ class Forum2Discourse::Models::SMF::Message
   belongs_to :topic, 'Forum2Discourse::Models::SMF::Topic'
   #belongs_to :board, 'Forum2Discourse::Models::SMF::Board'
   belongs_to :user, 'Forum2Discourse::Models::SMF::User'
-  has n, :attachments, 'Forum2Discourse::Models::SMF::Attachment'
+  #has n, :attachments, 'Forum2Discourse::Models::SMF::Attachment'
 
   def to_discourse
     return nil if topic==nil or topic.board==nil or topic.board.category==nil?
@@ -27,16 +27,16 @@ class Forum2Discourse::Models::SMF::Message
       raw: HTMLEntities.new.decode(body).gsub("[code]","\n```cpp\n").gsub("[/code]","\n```\n").gsub("<br/>","\n").gsub("<br />","\n"),
       created_at: created_at
     )
-    attachments.each do |attachment|
-        if File.extname(attachment.filename).downcase == "jpg" || File.extname(attachment.filename).downcase == "gif" || File.extname(attachment.filename).downcase == "jpeg" || File.extname(attachment.filename).downcase == "png"
-            raw = raw + "\n" + "<img src=\"" + attachment.url + "\"/>"
-        else
-            raw = raw + "\n" + "<a class=\"attachment\" href=\"" + attachment.url + "\">" + attachment.filename + "</a> (" + ("%0.2f" % attachment.size/1000) + ")"
-        end
-    end
+    #attachments.each do |attachment|
+    #    if File.extname(attachment.filename).downcase == "jpg" || File.extname(attachment.filename).downcase == "gif" || File.extname(attachment.filename).downcase == "jpeg" || File.extname(attachment.filename).downcase == "png"
+    #        raw = raw + "\n" + "<img src=\"" + attachment.url + "\"/>"
+    #    else
+    #        raw = raw + "\n" + "<a class=\"attachment\" href=\"" + attachment.url + "\">" + attachment.filename + "</a> (" + ("%0.2f" % attachment.size/1000) + ")"
+    #    end
+    #end
     
-    raw.gsub!("http://forum.openframeworks.cc/index.php/topic,([0-9]+)(.([0-9]+)?(.html)?(#msg[0-9]+)","http://forum.openframeworks.cc/t/l/\1")
-    raw.gsub!("http://forum.openframeworks.cc/index.php?topic=([0-9]+)(.([0-9]+)?(.html)?(#msg[0-9]+)","http://forum.openframeworks.cc/t/l/\1")
+    #raw.gsub!("http://forum.openframeworks.cc/index.php/topic,([0-9]+)(.([0-9]+)?(.html)?(#msg[0-9]+)","http://forum.openframeworks.cc/t/l/\1")
+    #raw.gsub!("http://forum.openframeworks.cc/index.php?topic=([0-9]+)(.([0-9]+)?(.html)?(#msg[0-9]+)","http://forum.openframeworks.cc/t/l/\1")
     return post
   end
 end
